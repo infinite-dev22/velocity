d704097f583e6500958d648a55407bf67caa2887
infinite-dev22 <mwigojm@gmail.com>
2025-01-05 05:27:34 +0300

    feat: Open packages for native image compilation
    Open all packages in the module for reflection to support native image compilation.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,4 +3,17 @@ module velo {
     requires com.fasterxml.jackson.databind;
     requires org.eclipse.jgit;
     exports inc.nomard.velo;
+    opens inc.nomard.velo;
+    exports inc.nomard.velo.cli;
+    opens inc.nomard.velo.cli;
+    exports inc.nomard.velo.exception;
+    opens inc.nomard.velo.exception;
+    exports inc.nomard.velo.git;
+    opens inc.nomard.velo.git;
+    exports inc.nomard.velo.model;
+    opens inc.nomard.velo.model;
+    exports inc.nomard.velo.templating;
+    opens inc.nomard.velo.templating;
+    exports inc.nomard.velo.validator;
+    opens inc.nomard.velo.validator;
 }
