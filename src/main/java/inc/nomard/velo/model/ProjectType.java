b8b150e6423e61baa854dad0f15ebe1cf056eaa7
infinite-dev22 <mwigojm@gmail.com>
2025-01-05 05:29:03 +0300

    feat: Add ProjectType enum
    Add a new enum called `ProjectType` to represent different project types. This enum includes values for Spring Boot, Flutter, Kotlin, React, and Swift.

---
+++ src/main/java/inc/nomard/velo/model/ProjectType.java
@@ -0,0 +1,8 @@
+package inc.nomard.velo.model;
+
+public enum ProjectType {
+    SPRINGBOOT("springboot"), FLUTTER("flutter"), KOTLIN("kotlin"), REACT("react"), SWIFT("swift");
+
+    ProjectType(String value) {
+    }
+}
