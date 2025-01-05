e4f8d16363a8d3112e206e48baef9a07be5cd3b6
infinite-dev22 <mwigojm@gmail.com>
2025-01-05 05:29:11 +0300

    feat: Add project types for the CLI
    This commit introduces a new class `ProjectTypes` which defines the supported project types for the CLI. It includes Spring Boot, Flutter, Kotlin, React, and Swift.

---
+++ src/main/java/inc/nomard/velo/cli/ProjectTypes.java
@@ -0,0 +1,17 @@
+package inc.nomard.velo.cli;
+
+import inc.nomard.velo.model.ProjectType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProjectTypes extends ArrayList<String> {
+    ProjectTypes() {
+        super(List.of(
+                ProjectType.SPRINGBOOT.toString(),
+                ProjectType.FLUTTER.toString(),
+                ProjectType.KOTLIN.toString(),
+                ProjectType.REACT.toString(),
+                ProjectType.SWIFT.toString()));
+    }
+}
