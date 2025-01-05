62749be8dd3163b1a78b64b30059d0f965b39df2
infinite-dev22 <mwigojm@gmail.com>
2025-01-05 05:28:17 +0300

    feat: Add ErrorCode enum
    Add an ErrorCode enum to represent various error codes that can occur during project creation and template handling. Each error code has a unique integer value associated with it.

---
+++ src/main/java/inc/nomard/velo/exception/ErrorCode.java
@@ -0,0 +1,22 @@
+package inc.nomard.velo.exception;
+
+public enum ErrorCode {
+    TEMPLATE_NOT_FOUND(1001),
+    INVALID_TEMPLATE_FORMAT(1002),
+    PROJECT_CREATION_FAILED(1003),
+    GIT_INITIALIZATION_FAILED(1004),
+    INVALID_PROJECT_TYPE(1005),
+    INVALID_PROJECT_PATH(1006),
+    PATH_ALREADY_EXISTS(1007),
+    INVALID_PROJECT_NAME(1008);
+
+    private final int code;
+
+    ErrorCode(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+}
