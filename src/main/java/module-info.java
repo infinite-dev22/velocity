2b438fcde071af6024821566ef22a1921ec825dd
infinite-dev22 <mwigojm@gmail.com>
2025-01-05 05:40:30 +0300

    refactor: Rename module to 'velocity'
    This commit renames the Java module from 'velo' to 'velocity'.
    
    This change affects all package exports and open statements in the `module-info.java` file, reflecting the new module name.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,19 +1,19 @@
-module velo {
+module velocity {
     requires info.picocli;
     requires com.fasterxml.jackson.databind;
     requires org.eclipse.jgit;
-    exports inc.nomard.velo;
-    opens inc.nomard.velo;
-    exports inc.nomard.velo.cli;
-    opens inc.nomard.velo.cli;
-    exports inc.nomard.velo.exception;
-    opens inc.nomard.velo.exception;
-    exports inc.nomard.velo.git;
-    opens inc.nomard.velo.git;
-    exports inc.nomard.velo.model;
-    opens inc.nomard.velo.model;
-    exports inc.nomard.velo.templating;
-    opens inc.nomard.velo.templating;
-    exports inc.nomard.velo.validator;
-    opens inc.nomard.velo.validator;
+    exports inc.nomard.velocity;
+    opens inc.nomard.velocity;
+    exports inc.nomard.velocity.cli;
+    opens inc.nomard.velocity.cli;
+    exports inc.nomard.velocity.exception;
+    opens inc.nomard.velocity.exception;
+    exports inc.nomard.velocity.git;
+    opens inc.nomard.velocity.git;
+    exports inc.nomard.velocity.model;
+    opens inc.nomard.velocity.model;
+    exports inc.nomard.velocity.templating;
+    opens inc.nomard.velocity.templating;
+    exports inc.nomard.velocity.validator;
+    opens inc.nomard.velocity.validator;
 }
